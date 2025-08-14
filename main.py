db6788cd0f4265b26cd1a4f6f059129f86807eca
Joaquín Ortubia <joaco.ortubiabarlaam@gmail.com>
2025-08-14 12:26:26 -0300

    Cambio de orientación a orientacion

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from graficos import obtener_datos_grafico_sudi
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
 
 app = FastAPI()
-
-# Modelo que define los datos que recibimos
 
 
 class InputData(BaseModel):
@@ -14,20 +12,11 @@ class InputData(BaseModel):
     orientacion: str
     tv: float
 
-# Endpoint que recibe los datos y devuelve el resultado
-
 
 @app.post("/calcular_luz")
 def calcular_luz(data: InputData):
-    # Acá va tu cálculo real, por ahora usamos algo básico
     resultado_lux = data.ancho * data.largo * data.altura * data.tv
-
-    # Datos ficticios para el gráfico (después los reemplazás con los reales)
-    grafico_data = {
-        "x": [1, 2, 3],
-        "y": [4, 5, 6],
-        "z": [7, 8, 9]
-    }
+    grafico_data = obtener_datos_grafico_sudi()
 
     return {
         "resultado_lux": resultado_lux,
