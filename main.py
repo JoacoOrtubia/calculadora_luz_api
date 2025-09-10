12819e3fdd8c92e268de85ba31281c8d7ae8ca03
Joaquín Ortubia <joaco.ortubiabarlaam@gmail.com>
2025-09-10 12:59:49 -0300

    Fix: ajustar main.py raíz para que Python encuentre el paquete app en Render

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from app.main import app
+import sys
+import os
+
+# agrega el path del proyecto para que Python encuentre el paquete app
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
